603e12127160204b85894a9b5ade50a641f32433
Himanshu Sachdeva <himanshu.sachdeva1011@gmail.com>
2023-09-01 12:29:44 +0530

    Create file_3.py

---
+++ file_3.py
@@ -0,0 +1,50 @@
+from PIL import Image
+import pandas as pd
+
+# Open the image using Pillow
+image_path = 'your_image.png'
+img = Image.open(image_path)
+
+# Convert the image to grayscale
+gray_img = img.convert('L')
+
+# Apply thresholding to create a binary image
+threshold = 200  # Adjust the threshold value as needed
+binary_img = gray_img.point(lambda p: p > threshold and 255)
+
+# Perform connected component labeling to identify objects (potential table cells)
+from scipy import ndimage
+label_objects, num_labels = ndimage.label(binary_img)
+
+# Find bounding boxes of labeled objects
+objects_boxes = ndimage.find_objects(label_objects)
+
+# Initialize a list to hold the table data
+table_data = []
+
+# Iterate through detected objects (potential table cells)
+for obj_box in objects_boxes:
+    obj = binary_img[obj_box]
+
+    # Extract the region of interest (ROI) for the potential table cell
+    roi = gray_img.crop(obj_box)
+
+    # Use a simple text extraction method for the ROI (replace with your logic)
+    cell_text = "Your_Text_Extraction_Method(roi)"  # Replace with your extraction method
+
+    cell_text = cell_text.strip()
+
+    # Split cell text by tab or comma to identify columns
+    cell_data = [cell.strip() for cell in cell_text.split('\t')]  # Assuming tab as column separator
+
+    if len(cell_data) > 1:
+        table_data.append(cell_data)
+
+# Convert the table data to a Pandas DataFrame
+table_df = pd.DataFrame(table_data)
+
+# Save the DataFrame as a CSV file
+csv_file_path = 'table_data.csv'
+table_df.to_csv(csv_file_path, index=False)
+
+print(f"Table extracted and saved as {csv_file_path}")
